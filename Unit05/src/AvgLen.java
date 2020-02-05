380b969914eac6cff37060a61d54003db18aed80
Shkev <39930235+Shkev@users.noreply.github.com>
2020-02-05 15:00:34 -0800

    adding Unit05

---
+++ Unit05/src/AvgLen.java
@@ -0,0 +1,9 @@
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+	   int len1 = a.length();
+	   int len2 = b.length();
+	   return (float)(len1 + len2) / 2;
+	}
+}
---
+++ Unit05/src/FirstLastVowel.java
@@ -0,0 +1,15 @@
+public class FirstLastVowel
+{
+	static char vowels[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+	public static String go( String a )
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (a.charAt(0) == vowels[i] || a.charAt(a.length() - 1) == vowels[i])
+			{
+				return "yes";
+			}
+		}
+		return "no";
+	}
+}
