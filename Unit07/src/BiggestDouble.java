5b0388c62463cbf1f6dc3181231b7c6fe6346980
Shkev <39930235+Shkev@users.noreply.github.com>
2020-02-19 14:49:08 -0800

    updating BiggestDouble

--- Unit07/src/BiggestDouble.java
+++ Unit07/src/BiggestDouble.java
@@ -28,11 +28,20 @@ public class BiggestDouble
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double[] arr = {one, two, three, four};
+		double max = arr[0];
+		for (int i  = 0; i < arr.length-1; i++)
+		{
+			if (Math.max(arr[i], arr[i+1]) > max)
+			{
+				max = Math.max(arr[i], arr[i+1]);
+			}
+		}
+		return max;
 	}
 
 	public String toString()
 	{
-	   return String.format("%f %f %f %f\n", one, two, three, four);
+	   return String.format("%f %f %f %f\n", one, two, three, four) + "\nbiggest = "+getBiggest()+"\n";
 	}
 }
