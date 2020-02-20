a98113e04a87006249ed0f11cf9900d647185ff7
Shkev <39930235+Shkev@users.noreply.github.com>
2020-02-20 15:00:47 -0800

    Update Triples.java

--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -25,21 +25,38 @@ public class Triples
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
-		int max = 0;
-
-
-
+		
 		return 1;
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
+		int a = 0, b = 0, c = 0;
+		for (int i = 0; i < number; i++)
+		{
+			for (int j = 0; j < number; j++)
+			{
+				if ((j % 2 == 1 && i % 2 == 1) || (j % 2 == 0 && i % 2 == 0))
+				{
+					continue;
+				}
+				for (int k = 0; k < number; k++)
+				{
+					if (c % 2 == 0)
+					{
+						continue;
+					}
+					if (Math.pow(i, 2) + Math.pow(j, 2)== Math.pow(k,2))
+					{
+						a = i;
+						b = j;
+						c = k;
+						output = output + a + " " + b + " " + " " + c + "\n"; 
+					}
+				}
+			}
+		}
 
 		return output+"\n";
 	}
