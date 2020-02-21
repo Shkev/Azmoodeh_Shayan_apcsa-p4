054ecf125add102b02a261a7d17194b374756384
Shkev <39930235+Shkev@users.noreply.github.com>
2020-02-21 14:28:03 -0800

    updating Triples

--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -25,25 +25,31 @@ public class Triples
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
-		
-		return 1;
+		int gcd = 0;
+		for (int i = 1; i <= a; i++)
+		{
+			if (i > b || i > c) break;
+			if (a % i == 0 && b % i == 0 && c % i == 0)
+				gcd = i;
+		}
+		return gcd;
 	}
 
 	public String toString()
 	{
 		String output="";
 		int a = 0, b = 0, c = 0;
-		for (int i = 0; i < number; i++)
+		for (int i = 1; i < number; i++)
 		{
-			for (int j = 0; j < number; j++)
+			for (int j = i; j < number; j++)
 			{
 				if ((j % 2 == 1 && i % 2 == 1) || (j % 2 == 0 && i % 2 == 0))
 				{
 					continue;
 				}
-				for (int k = 0; k < number; k++)
+				for (int k = j; k < number; k++)
 				{
-					if (c % 2 == 0)
+					if (k % 2 == 0 || greatestCommonFactor(i, j, k) > 1)
 					{
 						continue;
 					}
--- Unit07/src/TriplesRunner.java
+++ Unit07/src/TriplesRunner.java
@@ -3,11 +3,20 @@
 //Name -  
 
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class TriplesRunner
 {
    public static void main(String args[])
    {
+	   Scanner scan = new Scanner(System.in);
+	   
+	   System.out.print("Enter an upper limit :: ");
+	   int num = scan.nextInt();
+	   
+	   Triples test = new Triples(num);
+	   System.out.print(test);
+	   
 	   
    }
 }
