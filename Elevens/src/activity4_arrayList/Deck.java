a66d9da446bd20f3e2d015710e64d7e88c984404
Shkev <39930235+Shkev@users.noreply.github.com>
2020-03-06 13:59:30 -0800

    adding arraylist implementation to Elevens

---
+++ Elevens/src/activity4_arrayList/Deck.java
@@ -0,0 +1,136 @@
+package activity4_arrayList;
+import static java.lang.Math.*;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * The Deck class represents a shuffled deck of cards.
+ * It provides several operations including
+ *      initialize, shuffle, deal, and check if empty.
+ */
+public class Deck {
+
+	/**
+	 * cards contains all the cards in the deck.
+	 */
+	private List<Card> cards;
+
+	/**
+	 * size is the number of not-yet-dealt cards.
+	 * Cards are dealt from the top (highest index) down.
+	 * The next card to be dealt is at size - 1.
+	 */
+	private int size;
+
+	//default constructor
+	public Deck()
+	{
+		cards = new ArrayList<Card>();
+		size = 0;
+	}
+	
+	/**
+	 * Creates a new <code>Deck</code> instance.<BR>
+	 * It pairs each element of ranks with each element of suits,
+	 * and produces one of the corresponding card.
+	 * @param ranks is an array containing all of the card ranks.
+	 * @param suits is an array containing all of the card suits.
+	 * @param values is an array containing all of the card point values.
+	 */
+	public Deck(String[] ranks, String[] suits, int[] values) 
+	{
+		cards = new ArrayList<Card>();
+		size = cards.size();
+		int index = 0;
+		for (int i = 0; i < suits.length; i++)
+		{
+			for (int j = 0; j < ranks.length; j++)
+			{
+				cards.add(new Card(ranks[j], suits[i], values[j]));
+			}
+		}
+		this.shuffle();
+	}
+
+
+	/**
+	 * Determines if this deck is empty (no undealt cards).
+	 * @return true if this deck is empty, false otherwise.
+	 */
+	public boolean isEmpty() 
+	{
+		return size == 0;
+	}
+
+	/**
+	 * Accesses the number of undealt cards in this deck.
+	 * @return the number of undealt cards in this deck.
+	 */
+	public int size()
+	{
+		return size;
+	}
+
+	/**
+	 * Randomly permute the given collection of cards
+	 * and reset the size to represent the entire deck.
+	 */
+	public void shuffle() 
+	{
+		size = cards.size();
+		for(int k = size - 1; k > 0; k--)
+		{
+			int r = (int)(Math.random() * (k - 0)) + 0;
+			Card tmp = cards.get(k);
+			cards.set(k, cards.get(r));
+			cards.set(r, tmp);
+		}
+	}
+
+	/**
+	 * Deals a card from this deck.
+	 * @return the card just dealt, or null if all the cards have been
+	 *         previously dealt.
+	 */
+	public Card deal() 
+	{
+		if (size > 0)
+		{
+			size--;
+			return cards.get(size);
+		}
+		return null;
+	}
+
+	/**
+	 * Generates and returns a string representation of this deck.
+	 * @return a string representation of this deck.
+	 */
+	@Override
+	public String toString() {
+		String rtn = "size = " + size + "\nUndealt cards: \n";
+
+		for (int k = size - 1; k >= 0; k--) {
+			rtn = rtn + cards.get(k);
+			if (k != 0) {
+				rtn = rtn + ", ";
+			}
+			if ((size - k) % 2 == 0) {
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\nDealt cards: \n";
+		for (int k = cards.size() - 1; k >= size; k--) {
+			rtn = rtn + cards.get(k);
+			if (k != size) {
+				rtn = rtn + ", ";
+			}
+			if ((k - cards.size()) % 2 == 0) {
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\n";
+		return rtn;
+	}
+}
